ed506b501d9447a1074980c29d42b711109da3de
Plutzz <benjiongco@gmail.com>
2024-08-16 16:56:20 -0700

    Random Window stuff again

--- GMTKJam2024/Assets/_Project/_Scripts/ScaleTest.cs
+++ GMTKJam2024/Assets/_Project/_Scripts/ScaleTest.cs
@@ -1,34 +1,57 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScaleTest : MonoBehaviour
 {
     private int initWidth;
     private int prevWidth;
+    private int initHeight;
+    private int prevHeight;
+    [SerializeField] private TextMeshProUGUI text;
     private void Start()
     {
+        //BorderlessWindow.SetFramelessWindow();
+        //BorderlessWindow.MoveWindowPos(1600, 900);
         initWidth = Screen.width;
         prevWidth = Screen.width;
+        initHeight = Screen.height;
+        prevHeight = Screen.height;
     }
 
     private void Update()
     {
-        if (prevWidth != Screen.width)
+        if (Input.GetKey(KeyCode.L))
+        {
+            Screen.SetResolution(Screen.width + 16, Screen.height + 9, false);
+            //BorderlessWindow.MoveWindowPos(Screen.width + 16, Screen.height + 9);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            Screen.SetResolution(Screen.width - 16, Screen.height - 9, false);
+            //BorderlessWindow.MoveWindowPos(Screen.width - 16, Screen.height - 9);
+        }
+
+        text.text = Screen.width + ":" + Screen.height;
+
+        if (prevWidth != Screen.width || prevHeight != Screen.height)
         {
             Tween();
         }
         prevWidth = Screen.width;
+        prevHeight = Screen.height;
+
+
     }
 
     private void Tween()
     {
-        transform.DOKill();
+        //transform.DOKill();
         Debug.Log("Tween");
-        transform.DOScale(initWidth / (float)Screen.width / 2f, 1f);
-        //transform.DOScaleX(initWidth / (float)Screen.width, 1f);
-        //transform.DOScaleY(initWidth / (float)Screen.width, 1f);
-
+        //transform.DOScaleX(initXWidth / (float)Screen.width, 1f);
+        //transform.DOScaleY(initYWidth / (float)Screen.height, 1f);
+        transform.localScale = new Vector3(initWidth / (float)Screen.width, initHeight / (float)Screen.height, 1);
     }
 }
