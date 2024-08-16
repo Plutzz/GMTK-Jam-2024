8547a96a7db9390cee482cc402901cab96509cd7
Plutzz <benjiongco@gmail.com>
2024-08-16 12:57:38 -0700

    Yoffset

--- GMTKJam2024/Assets/_Project/_Scripts/BlockFallTrigger.cs
+++ GMTKJam2024/Assets/_Project/_Scripts/BlockFallTrigger.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class BlockFallTrigger : MonoBehaviour
 {
     [SerializeField] private int numLives = 3;
+    public GameObject ground;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Block"))
         {
             Debug.Log(numLives + " Lives Remaining");
             numLives--;
+            Destroy(collision.gameObject);
             if(numLives <= 0)
             {
                 Debug.Log("YOU LOSE");
+                Destroy(ground);
             }
         }
     }
--- GMTKJam2024/Assets/_Project/_Scripts/BlockSelector.cs
+++ GMTKJam2024/Assets/_Project/_Scripts/BlockSelector.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class BlockSelector : MonoBehaviour
 {
+    [SerializeField] private float yOffset = 3f;
     [SerializeField] private List<GameObject> blocks;
     private void Start()
     {
@@ -16,7 +18,16 @@ public class BlockSelector : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             int randomBlockIndex = Random.Range(0, blocks.Count);
-            Instantiate(blocks[randomBlockIndex]);
+            float maxY = 0f;
+            foreach(Transform child in transform)
+            {
+                if(child.transform.position.y > maxY)
+                {
+                    maxY = child.transform.position.y;
+                }
+            }
+            GameObject block = Instantiate(blocks[randomBlockIndex], transform);
+            block.transform.position = new Vector3(block.transform.position.x, maxY + yOffset, block.transform.position.z);
             Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
         }
     }
