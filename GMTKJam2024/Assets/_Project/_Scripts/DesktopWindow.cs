f1427cdb9894e771457e55408db1f401c822fabf
Plutzz <benjiongco@gmail.com>
2024-08-18 19:06:40 -0700

    window follows the player

--- GMTKJam2024/Assets/_Project/_Scripts/DesktopWindow.cs
+++ GMTKJam2024/Assets/_Project/_Scripts/DesktopWindow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DesktopWindow : MonoBehaviour
@@ -9,7 +10,7 @@ public class DesktopWindow : MonoBehaviour
     public SpriteRenderer rend;
     public float borderSize;
     [SerializeField] Vector2 initWindowSize;
-    public float minXSize = 0.25f, minYSize = 0.25f, maxXSize = 17.7f, maxYSize = 5f;
+    public float minXSize = 0.25f, minYSize = 0.25f, maxXSize = 17.7f, maxYSize = 5f, horizontalBounds = 9.5f, verticalBounds = 5.4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,37 @@ public class DesktopWindow : MonoBehaviour
             GameManager.Instance.activeWindow = this;
         }
         
+    }
+
+    private void Update()
+    {
+
+        if (applicationWindow && GameManager.Instance.player != null)
+        {
+            transform.position = GameManager.Instance.player.transform.position;
+
+            float _leftBound = transform.position.x - (rend.size.x / 2);
+            if(_leftBound < -horizontalBounds)
+            {
+                transform.position = new Vector2(-horizontalBounds + (rend.size.x / 2), transform.position.y);
+            }
+            float _rightBound = transform.position.x + (rend.size.x / 2);
+            if (_rightBound > horizontalBounds)
+            {
+                transform.position = new Vector2(horizontalBounds - (rend.size.x / 2), transform.position.y);
+            }
+
+            float _topBound = transform.position.y + (rend.size.y / 2);
+            if (_topBound > verticalBounds)
+            {
+                transform.position = new Vector2(transform.position.x, verticalBounds - (rend.size.y / 2));
+            }
+            float _bottomBound = transform.position.y - (rend.size.y / 2);
+            if (_bottomBound < -verticalBounds)
+            {
+                transform.position = new Vector2(transform.position.x, -verticalBounds + (rend.size.y / 2));
+            }
+        }
     }
 
     public void CloseWindow()
